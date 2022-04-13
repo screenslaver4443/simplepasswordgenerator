bc3000bb7c4f21a635c94beab38414204f83ff10
Nikolai <npesudovs@outlook.com>
2022-04-13 14:45:52 +0930

    created file, and added basics

---
+++ ver0.1.py
@@ -0,0 +1,8 @@
+import random
+pswdlength = int(input("How long do you want your password? "))
+pswdlengthdone = 0
+pswdlist = []
+alphabet= ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","1","2","3","4","5","6","7","8","9","!","@","#","$","%","^","&","*","(",")"]
+print(random.choice(alphabet))
+#while pswdlengthdone <= pswdlength:
+    #pswdlist.append(random.choice(alphabet))
